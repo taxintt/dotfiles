8b21ae6daeb1962dfdcd83191af7d8215655d176
taxintt <nthouse14@gmail.com>
2021-10-09 14:35:04 +0900

    Modify tiny thing

--- iterm2/init.sh
+++ iterm2/init.sh
@@ -6,4 +6,4 @@ CURRENT_DIR=$(cd "$(dirname "$0")"; pwd)
 echo "$CURRENT_DIR"
 
 # https://apple.stackexchange.com/questions/111534/iterm2-doesnt-read-com-googlecode-iterm2-plist
-ln -fs "$CURRENT_DIR/com.googlecode.iterm2.plist" ~/Library/Preferences/com.googlecode.iterm2.plist
+ln -s -v "$CURRENT_DIR/com.googlecode.iterm2.plist" ~/Library/Preferences/com.googlecode.iterm2.plist

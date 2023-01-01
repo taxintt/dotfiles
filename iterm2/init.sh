469e53effa153fcbf23bcd4c9569752f0d132e0d
taxintt <nthouse14@gmail.com>
2023-01-01 21:47:50 +0900

    good bye iterm2

--- iterm2/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ue
-
-CURRENT_DIR=$(cd "$(dirname "$0")"; pwd)
-echo "$CURRENT_DIR"
-
-# https://apple.stackexchange.com/questions/111534/iterm2-doesnt-read-com-googlecode-iterm2-plist
-ln -s -v "$CURRENT_DIR/com.googlecode.iterm2.plist" ~/Library/Preferences/com.googlecode.iterm2.plist

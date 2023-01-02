72219e0916a3298c6b2e9a0f06dd0d6e8dc15161
taxintt <nthouse14@gmail.com>
2023-01-02 17:37:06 +0900

    Update wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 
-wezterm.on("open-htop", function(win, pane)
-    win:perform_action(wezterm.action{ SplitVertical = { domain = "CurrentPaneDomain", args = { "htop", "-ps", }, }, }, pane)
+wezterm.on("open-htop-pane", function(window, pane)
+    window:perform_action(wezterm.action{ SplitVertical = { domain = "CurrentPaneDomain", args = { "htop" }, }, }, pane)
 end)
 
 return {
@@ -17,7 +17,7 @@ return {
     line_height = 1.1,
     default_cursor_style = "BlinkingBar",
     use_ime = true,
-    
+
     -- Tab Bar Options
 	enable_tab_bar = true,
 	hide_tab_bar_if_only_one_tab = true,
@@ -61,7 +61,7 @@ return {
 	quick_select_alphabet = "colemak",
     -- https://wezfurlong.org/wezterm/config/keys.html?highlight=key%20bindings#leader-key
     -- press leadner key and "after that" press another key
-	leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }, 
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }, 
 	keys = {
         -- { key = "r", mods = "LEADER", action = "ReloadConfiguration" },
         { key = "r", mods = "CTRL", action = "ReloadConfiguration" },
@@ -77,6 +77,8 @@ return {
 			mods = "CTRL",
 			action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
 		},
+        -- 
+        { key = "p", mods = "LEADER", action = wezterm.action({ EmitEvent = "open-htop-pane" })},
         --
 		{ key = "r", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
 		{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
@@ -86,7 +88,7 @@ return {
 		{ key = "c", mods = "LEADER", action = wezterm.action({ CopyTo = "Clipboard" }) },
 		{ key = "v", mods = "LEADER", action = wezterm.action({ PasteFrom = "Clipboard" }) },
         -- 
-        -- https://blog.katsyoshi.org/blog/2022/03/19/hello-wezterm/
-        { key = "p", mods = "LEADER", action = wezterm.action({ EmitEvent = "open-htop" })},
+        -- https://github.com/wez/wezterm/issues/641
+        { key = "q", mods = "LEADER", action = "ShowDebugOverlay" },
 	},
 }

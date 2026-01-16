cacd0b42d80e331fd00b3599cf556bcf040a1583
taxintt <taxintt@hatena.ne.jp>
2026-01-17 00:38:08 +0900

    remove make_symlink .config/wezterm/wezterm.lua

--- scripts/link.sh
+++ scripts/link.sh
@@ -27,6 +27,5 @@ make_symlink .gitconfig
 make_symlink .gitconfig.local
 make_symlink .config/starship.toml
 make_symlink .config/mise/config.toml
-make_symlink .config/wezterm/wezterm.lua
 make_symlink .czrc
 make_symlink changelog.config.js
